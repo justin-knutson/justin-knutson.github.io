0be71f56f5033c3e84206ff10745bb0d16eb5ff2
justin-knutson <72714945+justin-knutson@users.noreply.github.com>
2020-10-11 17:38:06 -0700

    Update raceIAT.js

--- minnoJS/raceIAT.js
+++ minnoJS/raceIAT.js
@@ -1,4 +1,4 @@
-define(['pipAPI', 'https://baranan.github.io/minno-tasks/quiat8.js'], function(APIConstructor, iatExtension){
+define(['pipAPI', 'https://baranan.github.io/minno-tasks/quiat9.js'], function(APIConstructor, iatExtension){
     var API = new APIConstructor();
 
 	return iatExtension({
@@ -42,15 +42,9 @@ define(['pipAPI', 'https://baranan.github.io/minno-tasks/quiat8.js'], function(A
 			image : 'https://baranan.github.io/minno-tasks/images/'
         },
         
-        showDebriefing:true, //Show feedback in the last trial? Relevant only in a Qualtrics IAT because in Qualtrics we cannot access the saved feedback and IAT score later in the survey.
+        showDebriefing : true, //Show feedback in the last trial? Relevant only in a Qualtrics IAT because in Qualtrics we cannot access the saved feedback and IAT score later in the survey.
         //Texts for the trials that show the debriefing.
-        preDebriefingText : 'Press space to see your result', //Text in the trial that comes before showing the debriefing.
-        preDebriefingTouchText : 'Touch the bottom green area to see your result', //Touch version for the text in the trial that comes before showing the debriefing.
-        debriefingTextTop : 'Your result:', //Will be shown above the feedback text.
-        //ATTENTION: We do not recommend showing participants their results. The IAT is a typical psychological measure so it is not very accurate. 
-        //In Project Implicit's website, you can see that we added much text to explain that there is still much unknown about the meaning of these results.
-        //We strongly recommend that you provide all these details in the debriefing of the experiment.
         debriefingTextBottom : '', //Will be shown below the feedback text. 
-
+        
 	});
 });
